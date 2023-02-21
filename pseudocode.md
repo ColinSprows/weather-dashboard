8fdea00bf42f55a4b7195c165d36a7af079c623b
Colin Sprows <colin.sprows@gmail.com>
2023-02-21 10:20:07 -0500

    added pseudocode

---
+++ pseudocode.md
@@ -0,0 +1,7 @@
+1. pull in weather data from openweather api
+
+2. define the current day and next 5 days
+
+3. define the selected city based on users search
+
+4. populate the page based on selected city/dates
---
+++ script.js
@@ -0,0 +1,17 @@
+// Searchbar
+    // user searches for and selects a city
+
+// Searchbar History
+    // shows users recent searches in order
+
+// Current Weather Card
+    // populates based on users selection in searchbar
+
+// 5-day Forecast
+    // populates based on users selection in searchbar
+
+// DEPENDENCIES
+
+// DATA
+
+// FUNCTIONS
