18908033f6e4aaf34651f535efd626cbdc45a9ab
Colin Sprows <colin.sprows@gmail.com>
2023-02-21 14:29:22 -0500

    accessed API and set up functions

--- script.js
+++ script.js
@@ -13,5 +13,32 @@
 // DEPENDENCIES
 
 // DATA
+var apiKey = "c88f55c4afe5f730770c8ce85607d5a9"
 
 // FUNCTIONS
+function getWeather(cityName) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data)
+        });
+}
+
+function searchButtonHandler(event) {
+    event.preventDefault();
+    let city = $("#city-input").val().trim();
+    getWeather(city);
+}
+
+function showWeather(weather) {
+
+}
+
+function searchHistory() {
+
+}
+
+// INTERACTIONS
+$("#city-button").click(searchButtonHandler)
