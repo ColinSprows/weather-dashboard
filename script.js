587a5a5632e6fc3898bbf5e2573b1af0015fc8e8
Colin Sprows <colin.sprows@gmail.com>
2023-02-21 17:11:48 -0500

    added some stuff but its broken and need to fix

--- script.js
+++ script.js
@@ -14,6 +14,7 @@
 
 // DATA
 var apiKey = "c88f55c4afe5f730770c8ce85607d5a9"
+var searchHistory = []
 
 // FUNCTIONS
 function getWeather(cityName) {
@@ -32,11 +33,25 @@ function searchButtonHandler(event) {
     getWeather(city);
 }
 
+function saveSearchHistory(city) {
+    searchHistory.push(city);
+    $("#search-history").append("<a href='#' class='list-group-item list-group-item-action' id='" + city + "'>" + city + "</a>")
+    localStorage.setItem("weatherSearchHistory", JSON.stringify(searchHistory));
+    searchHistory = JSON.parse(localStorage.getItem("weatherSearchHistory"));
+    if (!searchHistory) {
+        searchHistory = []
+    }
+    for(i = 0 ; i < searchHistory.length ;i++) {
+        $("#search-history").append("<a href='#' class='list-group-item list-group-item-action' id='" + searchHistory[i] + "'>" + searchHistory[i] + "</a>");
+    }  
+};
+saveSearchHistory();
+
 function showWeather(weather) {
 
 }
 
-function searchHistory() {
+function showForecast(forecast) {
 
 }
 
